9d9aa1405ede73c7474c962e74a05d15ef844774
Yoonis Ali <yoonisali24@Yooniss-Air.home>
2022-12-16 09:47:52 -0800

    add first passing test

---
+++ Bakery/Models/Vendor.cs
@@ -0,0 +1,27 @@
+namespace Bakery.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _vendorList = new List<Vendor> { };
+
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public int Id { get; }
+    // public List<Order> orderList { get; set; }
+
+    public Vendor(string name, string location)
+    {
+      Name = name;
+      Location = location;
+      _vendorList.Add(this);
+      Id = _vendorList.Count;
+      // Orders = new List<Order>{ };
+    }
+
+    public static void ClearAll()
+    {
+      _vendorList.Clear();
+    }
+
+  }
+}
--- BakeryTests/ModelTests/VendorTests.cs
+++ BakeryTests/ModelTests/VendorTests.cs
@@ -1,1 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+ using Bakery.Models;
+ using System.Collections.Generic;
+ using System;
+
+ namespace Bakery.Tests
+ {
+
+   [TestClass]
+   public class VendorTests : IDisposable
+   {
+
+     public void Dispose()
+     {
+       Vendor.ClearAll();
+     }
+
+     [TestMethod]
+     public void VendorConstructor_CreateInstanceOfVendor_Vendor()
+     {
+       Vendor newVendor = new Vendor("Bob's Bakery", "Texas");
+       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+     }
+
+   }
+ }
