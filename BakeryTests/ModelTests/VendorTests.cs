7c257b1788e2d250006a1d4b3c72a06b8f5aa68d
Yoonis Ali <yoonisali24@Yooniss-Air.home>
2022-12-16 09:50:59 -0800

    add second passing test

--- BakeryTests/ModelTests/VendorTests.cs
+++ BakeryTests/ModelTests/VendorTests.cs
@@ -1,26 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
- using Bakery.Models;
- using System.Collections.Generic;
- using System;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
 
- namespace Bakery.Tests
- {
+namespace Bakery.Tests
+{
 
-   [TestClass]
-   public class VendorTests : IDisposable
-   {
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
 
-     public void Dispose()
-     {
-       Vendor.ClearAll();
-     }
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
 
-     [TestMethod]
-     public void VendorConstructor_CreateInstanceOfVendor_Vendor()
-     {
-       Vendor newVendor = new Vendor("Bob's Bakery", "Texas");
-       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-     }
+    [TestMethod]
+    public void VendorConstructor_CreateInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Bob's Bakery", "Texas");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
 
-   }
- }
+    [TestMethod]
+    public void VendorName_ReturnsVendorsName_String()
+    {
+      string name = "Bob's Bakery";
+      string location = "Texas";
+      Vendor newVendor = new Vendor(name, location);
+      string nameOfVendor = newVendor.Name;
+      Assert.AreEqual(nameOfVendor, name);
+    }
+
+  }
+}
