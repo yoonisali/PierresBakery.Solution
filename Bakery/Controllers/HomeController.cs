22856bacace2544b7e1b476d0e1a217a2a6f387f
Yoonis Ali <yoonisali24@Yooniss-Air.home>
2022-12-16 11:56:06 -0800

    add splash page

---
+++ Bakery/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers 
+{
+  public class HomeController : Controllers
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
